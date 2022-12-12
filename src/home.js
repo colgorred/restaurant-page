96eb1a5b8eebba4444c155046e63bb5281a99961
colgorred <colgorred@gmail.com>
2022-12-12 18:03:56 +0900

    Create homepage content and add helpers module

---
+++ src/home.js
@@ -0,0 +1,23 @@
+import { createHtmlElement } from "./helpers";
+
+function createHomepage() {
+  const main = document.createElement("main");
+  const titleKanji = createHtmlElement("h1", null, ["titleKanji"], "四合わせ");
+  const title = createHtmlElement("h1", null, ["title"], "SHIAWASE RAMEN");
+  const hr = document.createElement("hr");
+  const caption = createHtmlElement(
+    "p",
+    null,
+    ["caption"],
+    "Exquisite. Authentic. Japanese Ramen"
+  );
+
+  main.appendChild(titleKanji);
+  main.appendChild(title);
+  main.appendChild(hr);
+  main.appendChild(caption);
+
+  return main;
+}
+
+export { createHomepage };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("webpack is working!");
+import { createHomepage } from "./home";
+
+const content = document.querySelector(".content");
+content.appendChild(createHomepage());
